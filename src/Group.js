0214f14b8ab3313865719bdc1d8bce0784f1709f
Redcamel <webseon@gmail.com>
2015-06-18 00:00:58 +0900

    그룹은 마일스톤이 밀려있으므로 삭제

--- src/Group.js
+++ src/Group.js
@@ -1,33 +1,13 @@
 var Group = Matrix.extend(function Group(){
-  this._Children = {};
+
 })
-.method('addChild', function addChild(id, mesh) { 
-    var k, checks;
-    if (this._children[id]) this.error(0);
-    if (!(mesh instanceof Mesh )) this.error(1);
-    mesh._scene = this,
-    mesh.setGeometry(mesh._geometry),
-    mesh.setMaterial(mesh._material),
-    checks = mesh._geometry._vertexShaders;
-    for (k in checks)
-        if (typeof checks[k] == 'string')
-            if (!this._scene._vertexShaders[checks[k]]) this.error(2);
-    checks = mesh._material._fragmentShaders;
-    for (k in checks)
-        if (typeof checks[k] == 'string')
-            if (!this._scene._fragmentShaders[checks[k]]) this.error(3);
-    checks = mesh._material._textures;
-    for (k in checks)
-        if (typeof checks[k] == 'string')
-            if (!this._scene._textures[checks[k]]) this.error(4);
-    this._children[id] = mesh;
+.method('addChild', function addChild(mesh) {
     return this;
 })
 .method('getChild', function getChild(id) { 
-    var t = this._children[id];
-    return t ? t : null;
+    return null
 })
 .method('removeChild', function removeChild(id) { 
-    return this._children[id] ? (delete this._children[id], true) : false;
+    return this
 })
 .build();
