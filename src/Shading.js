d7fc748d843e931b37b1aa3b9fe563f51f309da0
shallaa <shallaa@shallaazm.com>
2015-06-22 21:38:38 +0900

    호스트에서 사용되는 Shading도 MoGL을 상속하도록 처리.

--- src/Shading.js
+++ src/Shading.js
@@ -1,7 +1,29 @@
-var Shading = (function(){
-    'use strict';
-    var Shading = {}, key = 'none,gouraud,phong,blinn,flat,toon'.split(','), i = key.length;
-    while (i--) Shading[key[i]] = key[i];
-    Object.freeze(Shading);
-    return Shading;
-})();
+var Shading = MoGL.extend('Shading', {
+    description:'Shading',
+    value:function Shading() {}
+})
+.constant('none', {
+    description:'none constant',
+    value:'none'
+})
+.constant('gouraud', {
+    description:'gouraud constant',
+    value:'gouraud'
+})
+.constant('phong', {
+    description:'phong constant',
+    value:'phong'
+})
+.constant('blinn', {
+    description:'blinn constant',
+    value:'blinn'
+})
+.constant('flat', {
+    description:'flat constant',
+    value:'flat'
+})
+.constant('toon', {
+    description:'toon',
+    value:'toon'
+})
+.build();
